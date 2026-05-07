65485f713f8d5ec0bfe2fb0d2a69fda264b0fdc3
wajdi hcini <wajdihcinipro01@gmai.com>
2026-05-07 14:47:28 +0200

    modify-jwt-token

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controller/usersController');
-const verifyJwt = require('../middleware/verifyJwt');
+const verifyJWT = require('../middleware/verifyJWT');
 
 // we will access now via the prefix /users/
-router.use(verifyJwt); // Apply the JWT verification middleware to all routes in this router
+router.use(verifyJWT); // Apply the JWT verification middleware to all routes in this router
 
 router.route("/").get(userController.getALLUsers);
 
